a6f633105242e8890240d7a05aa02e544601b72c
Shantanu Bhattarai <shantanu.bhattarai271@gmail.com>
2020-08-01 15:25:33 +0545

    Updates

---
+++ precache-manifest.336e9c1934e53d324c1c40ee904e9149.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "352d829a9367ad5acacf36c73de06594",
+    "url": "/hnviewer/index.html"
+  },
+  {
+    "revision": "7a7a38f8c1cea31c055b",
+    "url": "/hnviewer/static/css/main.5a9f5950.chunk.css"
+  },
+  {
+    "revision": "8c533ea30e8255b4de68",
+    "url": "/hnviewer/static/js/2.b942f057.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/hnviewer/static/js/2.b942f057.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a7a38f8c1cea31c055b",
+    "url": "/hnviewer/static/js/main.61470e51.chunk.js"
+  },
+  {
+    "revision": "89bbb96ae248472fed77",
+    "url": "/hnviewer/static/js/runtime-main.3f609290.js"
+  }
+]);
